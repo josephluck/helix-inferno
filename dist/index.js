105cff68f50289c45d67a2bdf43c14e0407f87ca
Joseph Luck <joseph.luck@sky.com>
2017-03-12 12:39:46 +0000

    Setup typescript packaging as ES6 module

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inferno_1 = require("inferno");
-var h = require("inferno-create-element");
-exports.default = h;
-function renderer(dom) {
-    var _dom = dom;
+import inferno from 'inferno';
+import * as h from 'inferno-create-element';
+export default h;
+export function renderer(dom) {
+    let _dom = dom;
     return function (node, state, prev, actions) {
         if (node) {
-            inferno_1.default.render(node(state, prev, actions), _dom);
+            inferno.render(node(state, prev, actions), _dom);
         }
     };
 }
-exports.renderer = renderer;
